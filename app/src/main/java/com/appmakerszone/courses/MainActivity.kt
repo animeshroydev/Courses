d5cc87481209696e102b1fbc3aee7c6451488da6
animesh <royanimesh2211@gmail.com>
2024-08-26 18:37:38 +0530

    app done

--- app/src/main/java/com/appmakerszone/courses/MainActivity.kt
+++ app/src/main/java/com/appmakerszone/courses/MainActivity.kt
@@ -3,10 +3,30 @@ package com.appmakerszone.courses
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.appmakerszone.courses.data.DataSource
+import com.appmakerszone.courses.model.Courses
 import com.appmakerszone.courses.ui.theme.CoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,17 +40,86 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun CoursesApp(modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello Courses App",
-            modifier = modifier
-        )
+    fun CoursesApp() {
+
+            CoursesList(coursesList = DataSource().loadCourses())
+
     }
 
-    @Preview(showBackground = true)
+    @Composable
+    fun CoursesCard(courses: Courses, modifier: Modifier = Modifier) {
+
+        Card(
+            modifier = modifier
+                .height(100.dp)
+                .padding(10.dp)
+        ) {
+
+
+            Row {
+                Image(
+                    painter = painterResource(
+                        id = courses.imgResourceId
+                    ),
+                    contentDescription = stringResource(id = courses.stringResourceId),
+                    modifier = Modifier.width(68.dp).height(100.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                Column {
+                    Text(
+                        text = stringResource(id = courses.stringResourceId),
+                        modifier = Modifier.padding(
+                            start = 16.dp,
+                            end = 16.dp,
+                            top = 5.dp
+                        ),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+
+                    Row {
+
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_grain),
+                            contentDescription = null,
+                            modifier = Modifier.padding(
+                                start = 16.dp,
+                                top = 6.dp,
+                            )
+                        )
+
+                        Text(
+                            text = (courses.num).toString(),
+                            modifier = Modifier.padding(
+                                start = 16.dp,
+                                end = 16.dp,
+                                top = 10.dp,
+                            ),
+                            style = MaterialTheme.typography.labelMedium
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun CoursesList(coursesList: List<Courses>, modifier: Modifier = Modifier) {
+
+        LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+            items(coursesList) { courses ->
+                    CoursesCard(courses = courses)
+            }
+        }
+    }
+
+
+
+    @Preview(showBackground = true, showSystemUi = true)
     @Composable
     fun GreetingPreview() {
         CoursesTheme {
+//            CoursesCard(Courses(R.string.film, R.drawable.tech, 44))
             CoursesApp()
         }
     }
--- app/src/main/java/com/appmakerszone/courses/model/Courses.kt
+++ app/src/main/java/com/appmakerszone/courses/model/Courses.kt
@@ -1,9 +1,18 @@
 package com.appmakerszone.courses.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 data class Courses(
 
+    @StringRes
     val stringResourceId: Int,
+
+    val num: Int,
+
+    @DrawableRes
     val imgResourceId: Int,
-    val num: Int
+
+
 
 )
